92078c075d62dcc11d47f3f4f3fbca21e02ae9ee
phiamo <phiamo@gmail.com>
2012-05-01 03:31:39 +0200

    Added Buzz

--- DependencyInjection/MopaRemoteUpdateExtension.php
+++ DependencyInjection/MopaRemoteUpdateExtension.php
@@ -24,5 +24,7 @@ class MopaRemoteUpdateExtension extends Extension
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('security.yml');
     }
 }
